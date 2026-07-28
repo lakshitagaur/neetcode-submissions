3e7cc1ab730d8dd48a7a4b73b074ef77b0c7fa61
Lakshita Gaur <69597066+lakshitagaur@users.noreply.github.com>
2026-07-28 22:21:09 +0530

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int maxArea(int[] heights) {
+
+        int low =0;
+        int high = heights.length-1;
+        int maxArea = 0;
+        int area = 0; //diff of pointers  * lowest of the 2 pointers
+
+        while(low<high)
+        {
+            
+            int diff = high - low;
+
+            area  = diff * lowest(heights[low],heights[high]);
+
+            if(area > maxArea)
+            {
+                maxArea = area;
+            }
+            if(heights[low] < heights[high])
+            {
+                low++;
+            }
+            else
+            {
+                high--;
+            }
+
+        }
+        return maxArea;
+    }
+
+    private int lowest(int a, int b)
+    {
+        if( a > b)
+        {
+            return b;
+        }
+        return a;
+    }
+}
